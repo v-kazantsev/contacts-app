a852dfc017903aafff0aeb97149f4ee568134327
v-kazantsev <vvs201@mail.ru>
2023-09-12 13:08:16 +0300

    add store

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '@/store/store';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '@/styles/custom-theme';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}></ChakraProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ChakraProvider theme={theme}></ChakraProvider>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 )
