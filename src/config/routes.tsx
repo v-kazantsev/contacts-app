9927d6975e14a8493c6f7147f4a2e0776a0b9544
v-kazantsev <vvs201@mail.ru>
2023-09-12 13:17:16 +0300

    add router

---
+++ src/config/routes.tsx
@@ -0,0 +1,6 @@
+export const routes = [
+  {
+    path: "/",
+    element: <div>Main Page</div>,
+  },
+];
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import { store, persistor } from '@/store/store';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '@/styles/custom-theme';
 import './index.css';
+import { routes } from '@/config/routes';
+
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <ChakraProvider theme={theme}></ChakraProvider>
+        <ChakraProvider theme={theme}>
+          <RouterProvider router={router} />
+        </ChakraProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
