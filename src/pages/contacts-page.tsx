b4a0bd41f5dd201bd55eb5702eb95926b92b449d
v-kazantsev <vvs201@mail.ru>
2023-09-17 13:34:27 +0300

    implement logout

--- src/pages/contacts-page.tsx
+++ src/pages/contacts-page.tsx
@@ -1,12 +1,27 @@
 import { useEffect } from 'react';
+import { Box, Flex, Button } from '@chakra-ui/react';
 import { ContactsView } from '@/views/contacts-view';
 import { useAppDispatch } from '@/hooks/use-app-dispatch';
 import { getContacts } from '@/features/contacts/actions';
+import { authActions } from '@/features/auth/slice'
+import { useNavigate } from 'react-router-dom';
 
 export const ContactsPage = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   useEffect(() => {
     dispatch(getContacts());
-  }, [dispatch])
-  return <ContactsView />;
+  }, [dispatch]);
+  const handleLogout = () => {
+    dispatch(authActions.logout());
+    navigate("/");
+  };
+  return (
+    <Box>
+      <Flex justify="flex-end" borderBottom="1px solid gray" py="12px" pr="24px" mb="24px">
+        <Button onClick={handleLogout} colorScheme='blue'>ВЫХОД</Button>
+      </Flex>
+      <ContactsView />
+    </Box>
+  );
 };
